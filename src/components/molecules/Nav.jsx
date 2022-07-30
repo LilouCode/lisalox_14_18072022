bfcaa364225ca6b19066f632578a98d4c81bf73a
LilouCode <lisa.music@outlook.com>
2022-07-30 14:06:35 +0200

    feat: create nav molecule component

---
+++ src/components/molecules/Nav.jsx
@@ -0,0 +1,9 @@
+import {BtnNav} from '../atoms/buttons/BtnNav'
+export const Nav = () => {
+    return (
+        <div className="nav">
+            {/* <BtnNav content="Home" to="/"/>
+            <BtnNav content="List" to="/employeelist"/> */}
+        </div>
+    )
+}
