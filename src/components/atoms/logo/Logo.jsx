0ea0895db6019e7d4f2830f1a3bae060434a9dd4
LilouCode <lisa.music@outlook.com>
2022-07-31 17:54:00 +0200

    feat: create logos

---
+++ src/components/atoms/logo/Logo.jsx
@@ -0,0 +1,6 @@
+import WH from '../../../assets/images/WH.svg';
+export const Logo = () =>{
+    return(
+        <img className='logo' src={WH} alt="Logo Wealth Health"/>
+    )
+}
---
+++ src/components/atoms/logo/LogoHRnet.jsx
@@ -0,0 +1,6 @@
+import LogoHRnet from '../../../assets/images/LogoHRnet.svg';
+export const LogoHR = () =>{
+    return(
+        <img className='logoHRnet' src={LogoHRnet} alt="Logo Wealth Health"/>
+    )
+}
