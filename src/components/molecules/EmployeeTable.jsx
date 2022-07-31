10a8a871df0b920b2b32d8d6b1369ed648f6ebb2
LilouCode <lisa.music@outlook.com>
2022-07-31 17:54:20 +0200

    feat: create Table fils component

---
+++ src/components/molecules/EmployeeTable.jsx
@@ -0,0 +1,25 @@
+import {
+    Column,
+    Table,
+    useReactTable,
+    ColumnFiltersState,
+    getCoreRowModel,
+    getFilteredRowModel,
+    getFacetedRowModel,
+    getFacetedUniqueValues,
+    getFacetedMinMaxValues,
+    getPaginationRowModel,
+    sortingFns,
+    getSortedRowModel,
+    FilterFn,
+    SortingFn,
+    ColumnDef,
+    flexRender,
+    FilterFns,
+  } from '@tanstack/react-table'
+
+  export const EmployeeTable = () =>{
+    return(
+      <div></div>
+    )
+  }
