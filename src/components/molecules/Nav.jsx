5499045da2398f845e9352a13b11c9be26cc15d6
LilouCode <lisa.music@outlook.com>
2022-07-31 17:54:49 +0200

    feat: create Nav component

--- src/components/molecules/Nav.jsx
+++ src/components/molecules/Nav.jsx
@@ -2,8 +2,8 @@ import {BtnNav} from '../atoms/buttons/BtnNav'
 export const Nav = () => {
     return (
         <div className="nav">
-            {/* <BtnNav content="Home" to="/"/>
-            <BtnNav content="List" to="/employeelist"/> */}
+            <BtnNav content="Home" link="/"/>
+            <BtnNav content="List" link="/employeelist"/>
         </div>
     )
 }
